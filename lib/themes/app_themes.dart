dc40738b6a36e2c5646382ac176d8dd1ef4aec9b
OtavioMomberg <otavioaugusto777444@gmail.com>
2026-04-04 12:44:14 -0300

    "a class to control some themes of the app easier"

---
+++ lib/themes/app_themes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/painting.dart';
+
+class AppThemes {
+  static const gradient = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [
+      Color.fromARGB(255, 74, 75, 77),
+      Color.fromARGB(255, 46, 46, 47),
+      Color.fromARGB(255, 36, 37, 39)
+    ]
+  );
+}
